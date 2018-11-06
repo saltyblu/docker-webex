22cfd9f43cb6d4a34fc9796e0995f41a6d8a3c39
Volker Schmitz <v.schmitz01@gmail.com>
2018-11-06 10:53:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# docker container to run webex in linux
+
+## Setup
+
+first build the container locally with buld.sh
+
+```
+./build.sh
+```
+
+### execute webex.sh to start the container
+
+```
+./webex.sh
+```
+
+## Requirements:
+
+- linux
+- X11
+- xvhost
---
+++ webex.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#docker build -t webex .
+
+# for unix:
+xhost +local:
+
+docker run -ti \
+    --env DISPLAY="unix$DISPLAY" \
+    --privileged \
+    --volume /dev/snd:/dev/snd \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    webex "/usr/bin/firefox"
